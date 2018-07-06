6006995b5d7124eb0e91b8a13fc58f954ed28dc6
tianzhi <297037249@qq.com>
2018-07-06 11:17:49 +0800

    Update ImageSequence.js

--- pc/js/component/ImageSequence.js
+++ pc/js/component/ImageSequence.js
@@ -1,5 +1,5 @@
 /**
- * PC 端 游卡通行证组件
+ * 图片序列帧
  * *
  */
 
@@ -64,48 +64,4 @@
 
 			 
 		    var percent = Math.round(100 * this.store.length / this.maxLength);
-		    this.el.get(0).setAttribute('data-percent', percent);
-		    this.el.get(0).style.backgroundSize = percent + '% 100%';
-		    // 全部加载完毕，无论成功还是失败
-		    if (percent == 100) {
-		        var index = this.params.indexRange[0];
-		        this.el.get(0).innerHTML = '';
-		        // 依次append图片对象
-		        var step = function () {
-		            if (_this.store[index - 1]) {
-		                // _this.el.get(0).removeChild(_this.store[index - 1]);
-		                _this.el.empty()
-		            }    
-		            _this.el.get(0).append(_this.store[index]);
-		            // 序列增加
-		            index++;
-		            // 如果超过最大限制
-		            if (index <= _this.params.indexRange[1]) {
-		                setTimeout(step, 42);
-		            } else {
-		                // 本段播放结束回调
-		                typeof _this.params.callback == 'function' && _this.params.callback.call(_this,_this)
-		                
-		            }
-		        };
-		        // 等100%动画结束后执行播放
-		        setTimeout(step, 100);
-		    }
-		}
-	}	
-
-	$.fn.ImageSequence = function(options){
-		return $(this).each(function(){
-			if (!$(this).data('ImageSequence')) {
-	            $(this).data('ImageSequence', new ImageSequence($(this), options));
-	        }
-		})
-	}
-
-
-	return ImageSequence;
-
-}));
-
-
-
+		    this.el.get(0).setAttribute('data-percent',
